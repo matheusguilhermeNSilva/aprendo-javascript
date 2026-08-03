97de0934642389b383464f24a8bfb7b54cca2fa4
matheus guilherme nicacio da silva <matheusgui1213@gmail.com>
2026-08-03 20:58:21 -0300

    fest/aprendendo java

---
+++ javascript.js
@@ -0,0 +1,35 @@
+
+//calculo de idade
+let anoNascimento= 2006
+let atual = 2026
+const idade = atual - anoNascimento
+console.log("minha idade é " + idade + " anos")
+
+//mensagem
+const nome = "matheus"
+console.log("meu nome é " + nome  )
+
+//salrio
+let salario= 1500 ;
+let valeTransporte= 300;
+let vale = 800;
+let salarioDomes= salario + valeTransporte + vale 
+console.log("meu pratrimonio e R$"+ salarioDomes);
+
+//mensagem de localização
+let cidade="Curitiba"
+let estado="PR"
+let pais = "Brasil"
+console.log("eu vivo em "+ pais +"no estado de "+ estado+ " na cidade de "+ cidade )
+
+//tipo de variavel
+let temCarro = true
+
+console.log( typeof  temCarro)
+
+//simulacao bancaria
+let saldo = 0; //saldo inicial
+saldo +=200;//deposito
+saldo-=50; //saque
+console.log("meu saldo atual e R$"+saldo) ;
+
