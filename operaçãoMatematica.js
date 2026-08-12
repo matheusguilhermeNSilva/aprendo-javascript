63038ca500fe4c63809030cecaeb0999001f2d47
matheus guilherme nicacio da silva <matheusgui1213@gmail.com>
2026-08-11 21:45:30 -0300

    finalizando,operacao matematica,comparaçao e aprendedo and or not

---
+++ operaçãoMatematica.js
@@ -0,0 +1,48 @@
+const numeroUM = 4;
+const numeroDois = 8;
+
+const subtração = numeroDois - numeroUM;
+console.log("subtração: " + subtração);
+
+const soma= numeroUM + numeroDois;
+console.log("soma: " + soma);
+
+const divisão = numeroDois / numeroUM;
+console.log("divisão:"+ divisão);
+
+const multiplicação = numeroUM * numeroDois;
+console.log("multiplicação: " + multiplicação);
+
+const resto = numeroDois % numeroUM;
+console.log("resto: " + resto);
+
+const contaComplicada= numeroUM + numeroDois - divisão;
+console.log("conta complicada: " + contaComplicada);
+
+const contaComplicada2= numeroUM + (numeroDois * divisão);
+console.log("conta complicada 2: " + contaComplicada2);
+
+const potencia = 3 ** 2;
+console.log("potência: " + potencia);
+
+let contador = 0;
+//contador= contador + 1;
+contador++;
+contador++;
+console.log("incremento: " + contador);
+contador--;
+console.log("decremento: " + contador);
+
+let numeroTres = 5;
+numeroTres += 3; // equivalente a numeroTres = numeroTres + 3
+
+//numeroTres -= 2; // equivalente a numeroTres = numeroTres - 2
+//numeroTres *= 4; // equivalente a numeroTres = numeroTres * 4
+//numeroTres /= 2; // equivalente a numeroTres = numeroTres / 2
+//numeroTres %= 3; // equivalente a numeroTres = numeroTres % 3
+//numeroTres **= 2; // equivalente a numeroTres = numeroTres ** 2
+//numeroTres++; // equivalente a numeroTres = numeroTres + 1
+//numeroTres--; // equivalente a numeroTres = numeroTres - 1
+//numeroTres = numeroTres + 1; // equivalente a numeroTres++
+//numeroTres = numeroTres - 1; // equivalente a numeroTes--
+console.log("numeroTres: " + numeroTres);
